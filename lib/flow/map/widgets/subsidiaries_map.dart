4c0e9eecbff99d366abd4fd503fdadab504f55eb
dmacan <david.macan@zeromolecule.com>
2023-08-25 15:03:19 +0200

    feat: kroonchi

--- lib/flow/map/widgets/subsidiaries_map.dart
+++ lib/flow/map/widgets/subsidiaries_map.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -92,13 +91,8 @@ class SubsidiariesMap extends HookConsumerWidget {
       ),
       children: [
         TileLayer(
-          urlTemplate:
-              'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-          subdomains: const ['a', 'b', 'c', 'd'],
-          // Subdomains for the tile layer
-          retinaMode: kIsWeb,
-          additionalOptions: const {'ext': 'svg'},
-          tileDisplay: const TileDisplay.instantaneous(),
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: const ['a', 'b', 'c'],
         ),
         MarkerLayer(markers: markers),
       ],
