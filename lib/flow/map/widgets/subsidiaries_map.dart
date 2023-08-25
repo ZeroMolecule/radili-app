d39a0fa49190a8d78903f26d561f029028c844eb
dmacan <david.macan@zeromolecule.com>
2023-08-25 15:27:28 +0200

    fix: map

--- lib/flow/map/widgets/subsidiaries_map.dart
+++ lib/flow/map/widgets/subsidiaries_map.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -89,9 +88,6 @@ class SubsidiariesMap extends HookConsumerWidget {
           urlTemplate:
               'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
           subdomains: const ['a', 'b', 'c', 'd'],
-          // Subdomains for the tile layer
-          retinaMode: kIsWeb,
-          additionalOptions: const {'ext': 'svg'},
           tileDisplay: const TileDisplay.instantaneous(),
         ),
         MarkerLayer(markers: markers),
