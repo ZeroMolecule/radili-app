e56039231073c278ee2b27f255c51264ef60e851
dmacan <david.macan@zeromolecule.com>
2023-08-28 16:20:42 +0200

    fix: marker optimization

--- lib/providers/nearby_subsidiaries_provider.dart
+++ lib/providers/nearby_subsidiaries_provider.dart
@@ -1,10 +1,8 @@
-import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:radili/domain/data/subsidiary.dart';
 import 'package:radili/domain/repository/stores_repository.dart';
 import 'package:radili/providers/di/repository_providers.dart';
-import 'package:radili/util/extensions/iterable_extensions.dart';
 
 final nearbySubsidiariesProvider = StateNotifierProvider.autoDispose<
     NearbySubsidiariesProvider, AsyncValue<List<Subsidiary>>>((ref) {
@@ -30,15 +28,7 @@ class NearbySubsidiariesProvider
         northeast: northeast,
         southwest: southwest,
       );
-      state = AsyncData(
-        [...nearby, ...?state.valueOrNull]
-            .distinctBy((e) => e.id)
-            .sortedByCompare(
-              (e) => const Distance().distance(e.coordinates, center),
-              (a, b) => a.compareTo(b),
-            )
-            .toList(),
-      );
+      state = AsyncData(nearby);
     } catch (e, stack) {
       state = AsyncError<List<Subsidiary>>(e, stack).copyWithPrevious(state);
     }
