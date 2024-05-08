d04bb19b9b0689c12babbf674cad24c3aefe8d50
dmacan <david.macan@zeromolecule.com>
2024-05-08 09:48:22 +0200

    fix: subsidiaries map gestures

--- lib/flow/map/widgets/subsidiaries_map.dart
+++ lib/flow/map/widgets/subsidiaries_map.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -10,6 +11,7 @@ import 'package:radili/flow/map/providers/map_state.dart';
 import 'package:radili/flow/map/widgets/map_markers_layer.dart';
 import 'package:radili/flow/map/widgets/map_tile_layer.dart';
 import 'package:radili/flow/map/widgets/map_user_marker_layer.dart';
+import 'package:radili/hooks/breakpoints_hook.dart';
 import 'package:radili/hooks/map_controller_animated_hook.dart';
 import 'package:radili/hooks/on_value_changed_hook.dart';
 import 'package:radili/providers/location_provider.dart';
@@ -30,6 +32,7 @@ class SubsidiariesMap extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final breakpoints = useBreakpoints();
     final subsidiariesState = ref.watch(subsidiariesProvider(query));
     final controller = useMapControllerAnimated();
     final cameraBounds = useState<LatLngBounds?>(null);
@@ -95,13 +98,15 @@ class SubsidiariesMap extends HookConsumerWidget {
               mapStateNotifier.setState(dirty: true);
             }
           },
-          interactionOptions: const InteractionOptions(
+          interactionOptions: InteractionOptions(
             enableMultiFingerGestureRace: false,
             flags: InteractiveFlag.drag |
                 InteractiveFlag.pinchZoom |
                 InteractiveFlag.doubleTapZoom |
                 InteractiveFlag.flingAnimation |
-                InteractiveFlag.scrollWheelZoom,
+                ((breakpoints.isDesktop && kIsWeb)
+                    ? InteractiveFlag.scrollWheelZoom
+                    : 0),
           ),
           onTap: (_, __) => onSubsidiaryPressed(null),
         ),
